edf2acbf06012c36fca389a2a3f47f591ae211ad
hiho817 <kenny0817123@gmail.com>
2025-04-29 19:26:52 +0800

    Refactor Kilin class in fpga_server.hpp to enhance structure and readability

--- include/fpga_server.hpp
+++ include/fpga_server.hpp
@@ -2,8 +2,79 @@
 #define FPGA_SERVER_HPP
 
 #include "fpga_handler.hpp"
+#include "leg_module.hpp"
+#include "console.hpp"
+#include "fsm.hpp"
+
+#include <NodeHandler.h>
+#include <sys/time.h>
+#include <fstream>
+#include <yaml.h>
+#include <string>
+#include <vector>
+#include <mutex>
+
+#ifndef CONFIG_PATH
+// #define CONFIG_PATH "/home/admin/corgi_ws/fpga_driver/config/config.yaml"
+#define CONFIG_PATH "/home/hiho817/corgi_ws/fpga_driver/config/config.yaml"
+#endif
 
 void inthand(int signum);
 bool is_sys_stop();
+
+class Kilin{
+    public:
+        Kilin();
+
+        FpgaHandler fpga_;
+        ModeFsm fsm_;
+
+        YAML::Node yaml_node_;
+
+        /* console */
+        std::mutex main_mtx_;
+        Console console_;
+
+        /* interrupt config */
+        int main_irq_period_us_;
+        int can_irq_period_us_;
+
+        /* header msg */
+        struct timeval t_stamp;
+        int seq;
+
+        /* powerboard state */
+        std::vector<bool> powerboard_state_;
+        bool digital_switch_;
+        bool signal_switch_;
+        bool power_switch_;
+        bool NO_SWITCH_TIMEDOUT_ERROR_;
+        bool NO_CAN_TIMEDOUT_ERROR_;
+
+        /* robot state */
+        std::vector<LegModule> modules_list_;
+        ModeFsm fsm_;
+        bool HALL_CALIBRATED_;
+        int modules_num_;
+        int timeout_cnt_;
+        int max_timeout_cnt_;
+
+        void load_config_();
+
+        void interruptHandler(core::Subscriber<power_msg::PowerCmdStamped>& cmd_pb_sub_,
+            core::Publisher<power_msg::PowerStateStamped>& state_pb_pub_,
+            core::Subscriber<motor_msg::MotorCmdStamped>& cmd_sub_,
+            core::Publisher<motor_msg::MotorStateStamped>& state_pub_);
+
+        void powerboardPack(power_msg::PowerStateStamped &power_fb_msg);
+        
+        void mainLoop_(core::Subscriber<power_msg::PowerCmdStamped>& cmd_pb_sub_,
+            core::Publisher<power_msg::PowerStateStamped>& state_pb_pub_,
+            core::Subscriber<motor_msg::MotorCmdStamped>& cmd_sub_,
+            core::Publisher<motor_msg::MotorStateStamped>& state_pub_);
+        
+        void canLoop_();
+
+};
 
 #endif
